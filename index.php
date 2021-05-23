83f2fa7211e4e5f85a6b1fbfd33b087cd126e4df
Ningxin Li <ningxin.li91@gmail.com>
2021-05-22 21:15:08 -0400

    added index file

---
+++ index.php
@@ -0,0 +1,101 @@
+<head>
+	<meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>兼远教育</title>
+
+    <link href="favicon.png" rel="icon" type="image/png" />
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/styles.css">
+	<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+</head>
+<body>
+
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span>image</span>
+        <span class="fs-4">兼远教育</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+
+    <main class="px-3">
+	    <h1>冲破藩篱，立身未来</h1>
+	    <p class="lead">专业定制海外院校短期、长期课程</p>
+	    <p class="lead">
+	      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+	    </p>
+	</main>
+
+	<div class="container marketing">
+
+		<div class="row featurette">
+	      <div class="col-md-7">
+	        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+	        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+	      </div>
+	      <div class="col-md-5">
+	        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+	      </div>
+	    </div>
+
+
+		<div class="row">
+	      <div class="col-lg-6">
+	        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+	        <h2>线上项目</h2>
+	        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+	        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+	      </div><!-- /.col-lg-6 -->
+	      <div class="col-lg-6">
+	        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+	        <h2>实地项目</h2>
+	        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+	        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+	      </div><!-- /.col-lg-6 -->
+	      
+	    </div><!-- /.row -->
+
+
+	</div>
+
+
+	<footer class="mt-auto text-white-50">
+	    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+	</footer>
+
+</body>
