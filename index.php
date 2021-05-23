ff0bf239c927dc1a7c66c702a439a8b15927ee75
Ningxin Li <ningxin.li91@gmail.com>
2021-05-23 12:26:30 -0400

    tested

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <span>image</span>
-        <span class="fs-4">兼远教育</span>
+        <span class="fs-4">兼远教育 是白敬亭老婆做的</span>
       </a>
 
       <ul class="nav nav-pills">
